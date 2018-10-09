ef01ef5218bb2f5bf546426c7c6a102cf62dc1d9
Ha <haquanghuy@mail.ru>
2018-10-09 15:58:12 +0300

    lesson2Hard

---
+++ 2.js
@@ -0,0 +1,13 @@
+for (var i = 1; i < 11; i++) {
+document.write(i + '<br>');
+	}	
+
+var a = 20, b;
+b = prompt("Enter your age:");
+if (a == b) {
+	console.log("Hello!");
+}
+else
+{
+	console.log("Bye!");
+}
